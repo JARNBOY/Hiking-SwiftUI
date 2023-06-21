18ca89fa66a05a5702d3a835b25cc71668858815
Papon <papon.s@lawyerplusgroup.com>
2023-06-21 16:01:14 +0700

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Papon Supamongkonchai on 21/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
