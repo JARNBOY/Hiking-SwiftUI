e1a7c713b83db4e63e7a4449d87432f2b70b082c
Papon <papon.s@lawyerplusgroup.com>
2023-06-21 16:47:37 +0700

    [Hiking App Workshop] SwiftUI Card Component (Reviewed by Papon S.)

---
+++ Hiking/Utility/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Hiking
+//
+//  Created by Papon Supamongkonchai on 21/6/2566 BE.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+}
